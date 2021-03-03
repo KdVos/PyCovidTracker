3fc643ddad35f2c27aebdde2a49c96d2062a391f
KdVos <k.d.vos@student.tue.nl>
2021-03-03 23:04:58 +0100

    changes to formatting

--- covidtrack.py
+++ covidtrack.py
@@ -13,22 +13,22 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import date
 from datetime import time
-
 # Path to dataset, local file, and expected update time stamp
 url                         = "https://data.rivm.nl/covid-19/COVID-19_uitgevoerde_testen.json"
 file                        = "./COVID-19_uitgevoerde_testen.json"
 expectedUpdate              = time.fromisoformat('15:15:00')
+
 ##############################################################################
 # Check whether file needs to be retrieved, 
 # or whether old file is still the most up-to date one
 ##############################################################################
+
 fileExsist = os.path.isfile(file)
 if (fileExsist):
     fileStats  = os.stat(file)
     lastMod    = datetime.fromtimestamp(fileStats.st_mtime)
-    print("File last Modified:")
-    print(lastMod.date())
-# Retrieve file
+    print("File last Modified:",lastMod.date())
+
     timeToUpdate  =  datetime.today().combine(datetime.today(),expectedUpdate)
     timeLastUpdate=  timeToUpdate - timedelta(days=1)
 
@@ -46,15 +46,17 @@ if retrieveFile:
 # Read file
 with open(file) as f:
     data = json.load(f)        
+    
 ##############################################################################
 ##                           file statistics                                ##
 ##############################################################################
-print("First date of retrieved file: ")
+
 firstDataDate = date.fromisoformat(data[0]["Date_of_statistics"])
-print(firstDataDate)
-print("Last date of retrieved file: ")
 lastDataDate = date.fromisoformat(data[-1]["Date_of_statistics"])
-print(lastDataDate)
+
+print("First date of data: ",firstDataDate)
+print("Last date of data: ",lastDataDate)
+
 ##############################################################################
 ##                     Repeat most recent statistics                        ##
 ##############################################################################
@@ -69,9 +71,6 @@ for i in range(0,len(data),1):
         Tested    += data[i]["Tested_with_result"]
         TestedPos += data[i]["Tested_positive"]
 
-print("Tested")
-print(Tested)
-print("Tested Positive")
-print(TestedPos)
-print("Percentage")
-print(round((TestedPos/Tested)*100,2))
+print("Tested: ", Tested)
+print("Positive: ", TestedPos)
+print("Percentage: ", round((TestedPos/Tested)*100,2),'%')
