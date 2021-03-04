b997482767d7cf511fc36a453839602deacfc2bd
KdVos <k.d.vos@student.tue.nl>
2021-03-04 13:44:11 +0100

    improve structure

--- covidtrack.py
+++ covidtrack.py
@@ -5,72 +5,20 @@ Spyder Editor
 This is a temporary script file.
 """
 # Imports
-import urllib.request
-import json
-import os
+from functions import fileHandling
+import datetime 
 
-from datetime import datetime
-from datetime import timedelta
-from datetime import date
-from datetime import time
 # Path to dataset, local file, and expected update time stamp
 url                         = "https://data.rivm.nl/covid-19/COVID-19_uitgevoerde_testen.json"
 file                        = "./COVID-19_uitgevoerde_testen.json"
-expectedUpdate              = time.fromisoformat('15:15:00')
+expectedUpdate              = datetime.time.fromisoformat('15:15:00')
 
 ##############################################################################
 # Check whether file needs to be retrieved, 
 # or whether old file is still the most up-to date one
 ##############################################################################
-
-fileExsist = os.path.isfile(file)
-if (fileExsist):
-    fileStats  = os.stat(file)
-    lastMod    = datetime.fromtimestamp(fileStats.st_mtime)
-    print("File last Modified:",lastMod.date())
-
-    timeToUpdate  =  datetime.today().combine(datetime.today(),expectedUpdate)
-    timeLastUpdate=  timeToUpdate - timedelta(days=1)
-
-    retrieveAfterUpdate    =  (lastMod < timeToUpdate and datetime.today()>timeToUpdate)
-    retrieveAfterOutofDate =  lastMod < timeLastUpdate
-    
-    retrieveFile= retrieveAfterOutofDate or retrieveAfterUpdate    
-else:
-    print("File not present in path")
-    retrieveFile = True
 # Retrieve file
-if retrieveFile:
-    print("Retrieving File")
-    urllib.request.urlretrieve(url, file)
-# Read file
-with open(file) as f:
-    data = json.load(f)        
-    
+data = fileHandling.fileretrieval(file,url,expectedUpdate)
+# Print Stats from file
+fileHandling.printFileStats(data)
 ##############################################################################
-##                           file statistics                                ##
-##############################################################################
-
-firstDataDate = date.fromisoformat(data[0]["Date_of_statistics"])
-lastDataDate = date.fromisoformat(data[-1]["Date_of_statistics"])
-
-print("First date of data: ",firstDataDate)
-print("Last date of data: ",lastDataDate)
-
-##############################################################################
-##                     Repeat most recent statistics                        ##
-##############################################################################
-
-Tested    = 0
-TestedPos = 0
-
-for i in range(0,len(data),1):
-    date_i = date.fromisoformat(data[i]["Date_of_statistics"])
-   
-    if(date_i == lastDataDate):
-        Tested    += data[i]["Tested_with_result"]
-        TestedPos += data[i]["Tested_positive"]
-
-print("Tested: ", Tested)
-print("Positive: ", TestedPos)
-print("Percentage: ", round((TestedPos/Tested)*100,2),'%')
---
+++ functions/fileHandling.py
@@ -0,0 +1,67 @@
+import urllib.request
+import json
+import os
+
+import datetime
+
+#
+# fileretrieval function
+#
+def fileretrieval(file,url,expectedUpdate):
+    fileExsist = os.path.isfile(file)
+    if (fileExsist):
+        fileStats  = os.stat(file)
+        lastMod    = datetime.datetime.fromtimestamp(fileStats.st_mtime)
+        print("File last Modified:",lastMod.date())
+    
+        timeToUpdate  =  datetime.datetime.today().combine(datetime.datetime.today(),expectedUpdate)
+        timeLastUpdate=  timeToUpdate - datetime.timedelta(days=1)
+    
+        retrieveAfterUpdate    =  (lastMod < timeToUpdate and datetime.datetime.today()>timeToUpdate)
+        retrieveAfterOutofDate =  lastMod < timeLastUpdate
+        
+        retrieveFile= retrieveAfterOutofDate or retrieveAfterUpdate    
+    else:
+        print("File not present in path")
+        retrieveFile = True
+    # Retrieve file
+    if retrieveFile:
+        print("Retrieving File")
+        urllib.request.urlretrieve(url, file)
+    # Read file
+    with open(file) as f:
+        data = json.load(f)    
+        
+    return data
+
+#
+#    printFileStats function
+#
+def printFileStats(data):
+##############################################################################
+##                           file statistics                                ##
+##############################################################################
+
+    firstDataDate = datetime.date.fromisoformat(data[0]["Date_of_statistics"])
+    lastDataDate = datetime.date.fromisoformat(data[-1]["Date_of_statistics"])
+    
+    print("First date of data: ",firstDataDate)
+    print("Last date of data: ",lastDataDate)
+
+##############################################################################
+##                     Repeat most recent statistics                        ##
+##############################################################################
+
+    Tested    = 0
+    TestedPos = 0
+    
+    for i in range(0,len(data),1):
+        date_i = datetime.date.fromisoformat(data[i]["Date_of_statistics"])
+       
+        if(date_i == lastDataDate):
+            Tested    += data[i]["Tested_with_result"]
+            TestedPos += data[i]["Tested_positive"]
+    
+    print("Tested: ", Tested)
+    print("Positive: ", TestedPos)
+    print("Percentage: ", round((TestedPos/Tested)*100,2),'%')
